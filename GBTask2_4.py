f3cbce46f41ea3b287bf55072bd0d1169a14e2da
Egor Orlenok <messaf@mail.ru>
2022-12-06 23:32:51 +1000

    edit task2_4

--- GBTask2_4.py
+++ GBTask2_4.py
@@ -5,3 +5,31 @@
 # --> 0 2 3
 # -3 * -1 * 0 = 0
 # Вывод: 0
+
+import os
+clear = lambda: os.system('cls')
+clear()
+
+n = int(input('Введите число N: '))
+nNeg = n * -1
+
+numList = []
+
+for i in range(nNeg, n + 1):
+    numList.append(i)
+    
+print(numList)
+
+indexList = list(map(int, input('Введите индексы через пробел: ').split()))
+rMult = 0
+rMultStr = ''
+for i in indexList:
+    rMult *= numList[i]
+    print(rMult)
+    if i > 1:
+        rMultStr += ' * '
+    rMultStr += str(numList[i])
+
+# print(indexList)
+print(rMultStr + ' = ' + str(rMult))
+print(f'вывод: {rMult}')
