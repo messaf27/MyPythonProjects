4ac523a012838f3a8a01d5ac2b1ae26c1fc491e6
Egor Orlenok <messaf@mail.ru>
2023-01-23 16:20:56 +1000

    update calling algorithm

--- AlarmGSM_AlgTesting/alg_calling.py
+++ AlarmGSM_AlgTesting/alg_calling.py
@@ -9,14 +9,36 @@ contactList = [
     '+79563689378'
 ]
 
-diSate = {
-    'di-1' : False,
-    'di-2' : True,
-    'di-3' : False,
-    'di-4' : False,
-    'di-5' : False,
-    'di-6' : True
-}
+
+# diSate = {
+#     'di-1' : False,
+#     'di-2' : True,
+#     'di-3' : False,
+#     'di-4' : True,
+#     'di-5' : False,
+#     'di-6' : False
+# }
+
+class InputAlarm:
+    
+  curState = {  
+  'di-1' : False,
+  'di-2' : False,
+  'di-3' : False,
+  'di-4' : False,
+  'di-5' : False,
+  'di-6' : False    
+  }
+  
+  alrmCkeck = {  
+  'di-1' : False,
+  'di-2' : False,
+  'di-3' : False,
+  'di-4' : False,
+  'di-5' : False,
+  'di-6' : False    
+  }
+  
 
 class callMode(Enum):
   GENERAL = 0
@@ -25,10 +47,18 @@ class callMode(Enum):
 def DiCheck(di:dict):
     alCount = 0
     for key, value in di.items():
-        print(f"DI: {key}  State: {value} ")
+        # print(f"DI: {key}  State: {value} ")
         if(value == True):
             alCount += 1
     return alCount
+
+def GetAlarmDi(di:dict):
+    alStr = str()
+    for key, value in di.items():
+        # print(f"DI: {key}  State: {value} ")
+        if(value == True):
+            alStr += f"\nDI: {key}  State: {value}"
+    return alStr
     
 # alStatus = DiCheck(diSate)
     
@@ -45,7 +75,7 @@ def CallSubscriber(phone, di:dict):
     if(answer == 'y'):
         print('Accepted OK')
         print('Alarm DI:')
-        DiCheck(di)
+        print(GetAlarmDi(di))
         answerResult = True
     else: 
         print('Call declined')
@@ -57,7 +87,7 @@ def CallSubscriber(phone, di:dict):
 def SendMessageSubcsriber(phone, di:dict):
     print('============ SendMessageSubcsriber ============')    
     print(f'Alarm message to {phone}:') 
-    DiCheck(di)   
+    print(GetAlarmDi(di))   
     print('========= SendMessageSubcsriber End ===========')    
 
 def ClearAlarms(di:dict):
@@ -67,30 +97,44 @@ def ClearAlarms(di:dict):
     return di         
 
      
-def AlarmCalling(contact:list, mode:callMode, di:dict):
-    copyStatus = di.copy()
-    # DiCheck(copyStatus)
-    while(DiCheck(copyStatus) > 0): # Крутимся пока есть события
+def AlarmCalling(contact:list, mode:callMode, di:InputAlarm):
+    copyStatus = di.curState.copy()
 
-        for callnum in contact: # Перебираем лист с контактами
-            
+    while(DiCheck(copyStatus) > 0): # Крутимся пока есть события       
+
+        stopCall = False   
+
+        for i in range(len(contactList)):                                
             answStatus = False  # Статус ответа на вызов
             callCounter = 0     # Счетчик попыток дозвона
         
-            while((answStatus == False) and (callCounter < 3)): # Выполняем более 3 раз пока не ответил абонент
+            while((stopCall == False) and (answStatus == False) and (callCounter < 3)): # Выполняем более 3 раз пока не ответил абонент
                 callCounter += 1 
                 print(f'Call count: {callCounter}')
-                answStatus = CallSubscriber(callnum, copyStatus)
+                answStatus = CallSubscriber(contactList[i], copyStatus)
             if(answStatus == False):
-                SendMessageSubcsriber(callnum, copyStatus)
+                SendMessageSubcsriber(contactList[i], copyStatus)
+                if(i == (len(contactList) - 1)):
+                    copyStatus = ClearAlarms(copyStatus)
+                    print('Clear Alarm - > (answStatus == False)')      
+                        
             elif(answStatus == True):
-                if(mode.EXLUSIVE):
-                    # for key, value in copyStatus.items():
-                    #     if(value == True):
-                    #         copyStatus[key] = 'False'    
-                    copyStatus = ClearAlarms(copyStatus)   
-        if(mode.GENERAL):
-            copyStatus = ClearAlarms(copyStatus)                               
-            
+                if(mode == callMode.EXLUSIVE):
+                    copyStatus = ClearAlarms(copyStatus) 
+                    print('Clear Alarm - > (answStatus == True)')     
+                    return
+                                     
+            if(mode == callMode.GENERAL):
+                if(i == (len(contactList) - 1)):
+                    copyStatus = ClearAlarms(copyStatus)
+                    print('Clear Alarm - > (mode == callMode.GENERAL)')
+    return di.alrmCkeck                                    
         
-AlarmCalling(contactList, callMode.GENERAL, diSate)        
+# print(f'Contact list nums: {len(contactList)}')                   
+DevInput = InputAlarm()
+        
+AlarmCalling(contactList, callMode.GENERAL, DevInput.curState)       
+# AlarmCalling(contactList, callMode.EXLUSIVE, diSate) 
+
+print('Real alarm state:')
+print(GetAlarmDi(DevInput.curState)) 
